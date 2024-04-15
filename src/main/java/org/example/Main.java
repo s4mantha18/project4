46ab96e50c7795c528c8d5dcb3ca0fd64bd3613a
s4mantha18 <samanthaoloruntoba6@gmail.com>
2024-04-15 15:08:45 +0100

    first

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5};
+
+    }
+}
